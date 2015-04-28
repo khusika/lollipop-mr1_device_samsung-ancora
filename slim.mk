167cc353792951083732eec702ba147298b2b361
khusika <khusikadhamar@gmail.com>
2015-04-28 13:50:42 +0700

    changes for slim-lp5.1

--- slim.mk
+++ slim.mk
@@ -1,5 +1,5 @@
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common SlimRoms stuff.
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, $(LOCAL_PATH)/full_ancora.mk)
@@ -7,7 +7,7 @@ $(call inherit-product, $(LOCAL_PATH)/full_ancora.mk)
 PRODUCT_RELEASE_NAME := ancora
 
 # Setup device configuration
-PRODUCT_NAME := cm_ancora
+PRODUCT_NAME := slim_ancora
 PRODUCT_DEVICE := ancora
 PRODUCT_BRAND := Samsung
 PRODUCT_MANUFACTURER := Samsung
